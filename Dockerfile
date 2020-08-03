8b434fd8dfccc727ceb023da3fb5eebfe41e2f3d
David Chidell <david@chidell.com>
2020-08-03 13:56:43 +0100

    Added env var to abstract json key

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ENV ACME_PATH /acme
 ENV CERT_PATH /certs
 ENV ACME_FILE acme.json
 ENV SLEEP_TIME 86400
+ENV ACME_KEYS le
 
 COPY cert.py ./
 CMD ["python", "-u", "cert.py"]
--- cert.py
+++ cert.py
@@ -8,6 +8,7 @@ def main():
     cert_path = os.getenv('CERT_PATH', '/certs')
     acme_file = os.path.join(acme_path,os.getenv('ACME_FILE', 'acme.json'))
     sleep_time = int(os.getenv('SLEEP_TIME', '86400'))
+    acme_keys = os.getenv('ACME_KEY', 'le').split(',')
 
     acme_content_raw = ''
     print('Using acme file: '+acme_file)
@@ -16,32 +17,33 @@ def main():
 
     acme_content = json.loads(acme_content_raw)
 
-    certificates = acme_content['le']['Certificates']
+    for acme_key in acme_keys:
+        certificates = acme_content[acme_key]['Certificates']
 
-    print('Found '+str(len(certificates))+' certs.')
-    for cert in certificates:
-        domain = cert['domain']['main']
-        cert_b64 = cert['certificate']
-        key_b64 = cert['key']
-        chaincert = b64decode(cert_b64)
-        key = b64decode(key_b64)
-        cert = chaincert.split(b'\n\n')[0]
+        print('Found '+str(len(certificates))+' certs.')
+        for cert in certificates:
+            domain = cert['domain']['main']
+            cert_b64 = cert['certificate']
+            key_b64 = cert['key']
+            chaincert = b64decode(cert_b64)
+            key = b64decode(key_b64)
+            cert = chaincert.split(b'\n\n')[0]
 
-        print('Writing cert chain file: '+domain+'.chain.cer')
-        certchainfile = os.path.join(cert_path,domain+'.chain.cer')
-        with open(certchainfile,'wb') as fhnd:
-            fhnd.write(chaincert)
+            print('Writing cert chain file: '+domain+'.chain.cer')
+            certchainfile = os.path.join(cert_path,domain+'.chain.cer')
+            with open(certchainfile,'wb') as fhnd:
+                fhnd.write(chaincert)
 
-        print('Writing cert file: '+domain+'.cer')
-        certfile = os.path.join(cert_path,domain+'.cer')
-        with open(certfile,'wb') as fhnd:
-            fhnd.write(cert)
+            print('Writing cert file: '+domain+'.cer')
+            certfile = os.path.join(cert_path,domain+'.cer')
+            with open(certfile,'wb') as fhnd:
+                fhnd.write(cert)
 
-        print('Writing key file: '+domain+'.key')
-        keyfile = os.path.join(cert_path,domain+'.key')
-        with open(keyfile,'wb') as fhnd:
-            fhnd.write(key)
-        
+            print('Writing key file: '+domain+'.key')
+            keyfile = os.path.join(cert_path,domain+'.key')
+            with open(keyfile,'wb') as fhnd:
+                fhnd.write(key)
+            
     
     if sleep_time < 1:
         print('Sleep time below 1 second, exiting!')
